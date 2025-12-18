48ebc1faef42b7ba0c534bd8219fb851497a0d5d
Angelo Ferraz Modanez <angelomodanez15@gmail.com>
2025-12-18 16:18:59 -0300

    feat: table list close #18

--- src/app/components/car-table/car-table.ts
+++ src/app/components/car-table/car-table.ts
@@ -7,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrl: './car-table.css',
 })
 export class CarTable {
-
+  cars = [
+    {
+      id: 1,
+      name: 'Mustang',
+      maker: 'Autozone',
+      price: 40500.25,
+      year: 2005
+    },
+    {
+      id: 2,
+      name: 'Fusca',
+      maker: 'Ferrari',
+      price: 20500.25,
+      year: 2020
+    },
+    {
+      id: 3,
+      name: 'Coisa',
+      maker: 'coisa',
+      price: 0,
+      year: 0
+    },
+  ]
 }
---
+++ src/app/models/CarModel.ts
@@ -0,0 +1,7 @@
+export interface CarModel {
+  id: number;
+  name: string;
+  maker: string
+  price: number;
+  year: number;
+}
